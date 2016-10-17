6efe6dfedeece3e2c8e2092abe5bc0cc20926c24
Tracy <fernt877@newschool.edu>
2016-10-17 18:09:23 +0000

    NewComp

---
+++ jssintro/scripts.js
@@ -0,0 +1,27 @@
+var blackjack = 21;
+console.log(blackjack);
+console.log("another log...");
+
+var ae = 17;
+var name = "Miles";
+var parentsPresent = false;
+
+if (age >=18 || parentsPresent == true) {
+    console.log("come on in...");
+    } else {
+        if (age == 17) {
+            console.log("come back next year"); 
+    } 
+    console.log("come back when ur 18, or bring ur parents dude"); 
+}
+
+var myName = "Frank";
+var age = 17;
+
+if (myName = "Brendan") {
+    console.log("your name is brendan");
+}
+
+if (age == 17) {
+    console.log("come back next yr");
+}
